d510bb1d7f458e3570651fdb3c6fe9ca211ea38a
israel-duff <duff_israel@yahoo.com>
2022-02-08 23:48:29 +0100

    added public folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IncomeTaxModule,  } from './income-tax/income-tax.module';
 
 @Module({
-  imports: [IncomeTaxModule],
+  imports: [IncomeTaxModule, ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..','public'),
+    exclude: ['/tax*']
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/income-tax/income-tax.controller.ts
+++ src/income-tax/income-tax.controller.ts
@@ -1,16 +1,21 @@
-import { Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 import { IncomeTaxService } from "./income-tax.service";
 
 @Controller('tax')
 export class IncomeTaxController {
     constructor(private incomeTaxService: IncomeTaxService) {}
-    @Get()
-    incomeTax(): number {
-        return this.incomeTaxService.computeTaxableIncome(3000000, [1, 1, 1]);
+    @Post()
+    incomeTax(@Body() body: any): number {
+        // console.log('BODY:', body)
+        let b = [body.exemptions['c1'], body.exemptions['c2'], body.exemptions['c3']]
+        // return this.incomeTaxService.computeTaxableIncome(3000000, [1, 1, 1]);
+        return this.incomeTaxService.computeTaxableIncome(body.a, b);
     }
 
-    @Get('per/anum')
-    incomeTaxPerAnum(): number {
-        return this.incomeTaxService.computeAnualTaxPerAnum(3000000, [1, 1, 1]);
+    @Post('per/anum')
+    incomeTaxPerAnum(@Body() body: any): number {
+        let b = [body.exemptions['c1'], body.exemptions['c2'], body.exemptions['c3']]
+        // return this.incomeTaxService.computeAnualTaxPerAnum(3000000, [1, 1, 1]);
+        return this.incomeTaxService.computeAnualTaxPerAnum(body.a, b);
     }
 }
